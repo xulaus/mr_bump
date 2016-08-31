18b1e62fd5b4fb1c1bdb2116f97ba1a3ff27abf4
Richard Fitzgerald <richard.fitzgerald@intellectionsoftware.com>
2016-08-31 13:19:11 +0100

    Add licence header where missing from new files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 require 'bundler/setup'
 Bundler.setup
 
-require 'mr_bump' # and any other gems you need
+require 'mr_bump'
