37ea49053bfb0e6d7c738000d6f5842dcb3a4bbf
Richard Fitzgerald <richard.fitzgerald@intellectionsoftware.com>
2017-02-27 17:55:30 +0000

    DOOOOT

--- lib/mr_bump/config.rb
+++ lib/mr_bump/config.rb
@@ -23,8 +23,9 @@ module MrBump
 
     def default_config
       @default_config = begin
+        special_config = { 'projects' => Hash.new('Miscellaneous') }
         defaults_yml = File.join(File.dirname(__FILE__), '..', '..', 'defaults.yml')
-        YAML.load_file(defaults_yml)
+        special_config.merge(YAML.load_file(defaults_yml))
       end
     end
 
@@ -43,7 +44,17 @@ module MrBump
     end
 
     def config
-      @config ||= default_config.merge(user_config).merge(project_config)
+      @config ||= deep_merge(deep_merge(default_config, user_config), project_config)
+    end
+
+    def deep_merge(hash1, hash2)
+      return hash2 unless (hash2.is_a?(Hash) && hash1.is_a?(Hash)) || hash2.nil?
+      return hash1 if hash2.nil?
+      hash1.dup.tap do |master|
+        hash2.keys.each do |key|
+          master[key] = deep_merge(master.fetch(key, nil), hash2.fetch(key, nil))
+        end
+      end
     end
   end
 end
